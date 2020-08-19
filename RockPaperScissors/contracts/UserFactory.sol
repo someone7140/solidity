6a31ec1ddd51bf6934bba85d5a2c9d95a5451c83
someone7140 <ti7140@mail.goo.ne.jp>
2020-08-20 02:29:08 +0900

    送金箇所のロジック修正

--- RockPaperScissors/contracts/UserFactory.sol
+++ RockPaperScissors/contracts/UserFactory.sol
@@ -3,20 +3,9 @@ pragma solidity >=0.4.22 <0.8.0;
 
 contract UserFactory {
     mapping(address => string) userNameMapping;
-    mapping(address => uint256) userAmountMapping;
 
     // ユーザの作成
-    function _createUser(
-        address _userAddress,
-        string memory _name,
-        uint256 _amount
-    ) public {
+    function _createUser(address _userAddress, string memory _name) public {
         userNameMapping[_userAddress] = _name;
-        userAmountMapping[_userAddress] = _amount;
-    }
-
-    // 保有金額の確認
-    function _showAmount(address _userAddress) public view returns (uint256) {
-        return userAmountMapping[_userAddress];
     }
 }
